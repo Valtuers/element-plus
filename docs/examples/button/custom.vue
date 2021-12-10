361165272d9bdf3f329fe10344fa0f0eabfd3b76
Valtuers <32766590+Valtuers@users.noreply.github.com>
2021-12-10 15:35:16 +0800

    Update custom.vue

--- docs/examples/button/custom.vue
+++ docs/examples/button/custom.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row>
     <el-button color="#626aef" style="color: white">Custom</el-button>
-    <el-button color="#626aef" plain>Custom</el-button>
+    <el-button color="red" plain>Custom</el-button>
   </el-row>
 </template>
